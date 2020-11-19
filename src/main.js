bb077092d472cc0a1f17ea92783d2689108dbd20
miladMohammadi <afraaliansite@gmail.com>
2020-11-19 01:11:26 -0800

    خ

--- src/main.js
+++ src/main.js
@@ -8,6 +8,17 @@ Vue.use(MdButton)
 Vue.use(MdContent)
 Vue.use(MdTabs)
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
 new Vue({
     el: "#app",
     data: {
